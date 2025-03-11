23ae39cd7d7621cf464ac97db32714c11b148e99
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-03-11 18:53:56 +0000

    adding a class for the binomial model

---
+++ binomial_world/includes/BinomialModel.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "BinomialLattice.hpp"
+#include "BinomialRiskNeutralProbability.hpp"
+#include "Spot.hpp"
+
+class BinomialModel
+{
+private:
+    Spot S0;
+    size_t N{};
+    BinomialRiskNeutralProbability q;
+    BinomialLattice<double> lattice;
+    bool built{false};
+
+    void buildLattice();
+
+public:
+    BinomialModel() = default;
+    BinomialModel(double, double, double, size_t);
+    double getR() const;
+    double riskNeutralProbability() const;
+    const BinomialLattice<double> &getLattice() const;
+    const double &operator()(size_t, size_t) const;
+};
---
+++ binomial_world/src/BinomialModel.cpp
@@ -0,0 +1,50 @@
+#include "BinomialModel.hpp"
+#include <cassert>
+
+BinomialModel::BinomialModel(double newS0, double newU, double newR, size_t newN) : S0(newS0), q(newU, newR), N(newN), lattice(N)
+{
+    buildLattice();
+    built = true;
+}
+
+double BinomialModel::getR() const
+{
+    return q.getR();
+}
+
+double BinomialModel::riskNeutralProbability() const
+{
+    return q.getQ();
+}
+
+void BinomialModel::buildLattice()
+{
+    double U = q.getU();
+    double D = q.getD();
+
+    lattice(0, 0) = S0();
+
+    for (size_t i = 1; i <= N; i++)
+    {
+        for (size_t j = 0; j < i + 1; j++)
+        {
+            if (j == 0)
+                lattice(i, j) = lattice(i - 1, j) * (1. + D);
+            else if (j == i)
+                lattice(i, j) = lattice(i - 1, j - 1) * (1. + U);
+            else
+                lattice(i, j) = lattice(i - 1, j - 1) * (1. + U);
+        }
+    }
+}
+
+const BinomialLattice<double> &BinomialModel::getLattice() const
+{
+    assert(built);
+    return lattice;
+}
+
+const double &BinomialModel::operator()(size_t i, size_t j) const
+{
+    return lattice(i, j);
+}
