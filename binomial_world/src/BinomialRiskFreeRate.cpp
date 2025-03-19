06139e1f12e1d7124b5c5f745a6208c999c16d8f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-03-19 19:59:27 +0000

    cleaning old code

--- binomial_world/src/BinomialRiskFreeRate.cpp
+++
@@ -1,8 +0,0 @@
-#include "BinomialRiskFreeRate.hpp"
-#include <cassert>
-
-BinomialRiskFreeRate::BinomialRiskFreeRate(double newU, double newR) : directions(newU)
-{
-    assert(directions.getD() < newR && newR < directions.getU());
-    R = newR;
-}
