af0b646b69adad55d8a55f61edf77f881947ac43
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-03-29 05:39:47 +0000

    putting the code in the right place

--- binomial_world/includes/common.hpp
+++ binomial_world/includes/common.hpp
@@ -1,11 +1,31 @@
 #pragma once
+#ifndef BINOMIAL_WORLD_COMMON_HPP
+#define BINOMIAL_WORLD_COMMON_HPP
 #include <iostream>
 #include <vector>
 
+#include "BinomialLattice.hpp"
+
+template <typename T>
+std::ostream &operator<<(std::ostream &, const std::vector<T> &);
+
+template <typename T>
+std::ostream &operator<<(std::ostream &, const BinomialLattice<T> &);
+
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &vector) {
-    for (auto &elem : vector) {
-        os << elem << " ";
+    for (const T &element : vector) {
+        os << element << " ";
     }
     return os;
 }
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const BinomialLattice<T> &Lattice) {
+    for (const std::vector<T> &row : Lattice.getLattice()) {
+        os << row << "\n";
+    }
+    return os;
+}
+
+#endif  // BINOMIAL_WORLD_COMMON_HPP
