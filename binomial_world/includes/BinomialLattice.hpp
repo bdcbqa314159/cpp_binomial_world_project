39eee1f4959601a83bd47e068a1648ec54d84675
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-03-29 05:35:48 +0000

    putting the code in the right place

--- binomial_world/includes/BinomialLattice.hpp
+++ binomial_world/includes/BinomialLattice.hpp
@@ -4,72 +4,53 @@
 
 #include <cassert>
 #include <cstddef>
-#include <iostream>
-#include <ostream>
 #include <vector>
 
 // Fundamental block of the binmomial model - the binomial lattice as a data
 // structure
-
 template <typename T>
 class BinomialLattice {
    private:
-    size_t N{};
-    std::vector<std::vector<T>> Lattice;
+    size_t periods{};
+    std::vector<std::vector<T>> lattice;
 
    public:
-    BinomialLattice() = default;
     BinomialLattice(size_t);
+    size_t getPeriods() const;
+    const std::vector<std::vector<T>> &getLattice() const;
 
-    size_t size() const;
-    void resize(size_t newN);
     const std::vector<T> &operator[](size_t) const;
-    std::vector<T> &operator[](size_t i);
+    std::vector<T> &operator[](size_t);
 };
 
 template <typename T>
-void BinomialLattice<T>::resize(size_t newN) {
-    N = newN;
-    Lattice.resize(N + 1);
-    for (size_t i = 0; i < N + 1; i++) {
-        Lattice[i].resize(i + 1);
+BinomialLattice<T>::BinomialLattice(size_t newPeriods) : periods(newPeriods) {
+    lattice.resize(periods + 1);
+    for (size_t i = 0; i <= periods; i++) {
+        lattice[i].resize(i + 1);
     }
-}
-
-template <typename T>
-BinomialLattice<T>::BinomialLattice(size_t newN) : N(newN) {
-    Lattice.resize(N + 1);
-    for (size_t i = 0; i < N + 1; i++) {
-        Lattice[i].resize(i + 1);
-    }
-}
-
-template <typename T>
-size_t BinomialLattice<T>::size() const {
-    return N;
 }
 
 template <typename T>
 const std::vector<T> &BinomialLattice<T>::operator[](size_t i) const {
-    assert(i <= N);
-    return Lattice[i];
+    assert(i <= periods);
+    return lattice[i];
 }
 
 template <typename T>
 std::vector<T> &BinomialLattice<T>::operator[](size_t i) {
-    assert(i <= N);
-    return Lattice[i];
+    assert(i <= periods);
+    return lattice[i];
 }
 
 template <typename T>
-std::ostream &operator<<(std::ostream &os, const BinomialLattice<T> &Lattice) {
-    for (size_t i = 0; i <= Lattice.size(); i++) {
-        for (size_t j = 0; j < i + 1; j++) {
-            os << Lattice[i][j] << " ";
-        }
-        os << std::endl;
-    }
-    return os;
+size_t BinomialLattice<T>::getPeriods() const {
+    return periods;
+}
+
+template <typename T>
+const std::vector<std::vector<T>> &BinomialLattice<T>::getLattice() const {
+    return lattice;
 }
 
 #endif  // BINOMIALLATTICE_HPP
--- binomial_world/includes/refactoring_ter.hpp
+++ binomial_world/includes/refactoring_ter.hpp
@@ -2,56 +2,58 @@
 #ifndef BINOMIAL_WORLD_REFACTORING_TER_HPP
 #define BINOMIAL_WORLD_REFACTORING_TER_HPP
 
-#include <cassert>
 #include <iostream>
 #include <vector>
 
+#include "BinomialLattice.hpp"
+
 namespace new_code_bis {
 
-template <typename T>
-class BinomialLattice {
-   private:
-    size_t periods{};
-    std::vector<std::vector<T>> lattice;
+// template <typename T>
+// class BinomialLattice {
+//    private:
+//     size_t periods{};
+//     std::vector<std::vector<T>> lattice;
 
-   public:
-    BinomialLattice(size_t);
-    size_t getPeriods() const;
-    const std::vector<std::vector<T>> &getLattice() const;
+//    public:
+//     BinomialLattice(size_t);
+//     size_t getPeriods() const;
+//     const std::vector<std::vector<T>> &getLattice() const;
 
-    const std::vector<T> &operator[](size_t) const;
-    std::vector<T> &operator[](size_t);
-};
+//     const std::vector<T> &operator[](size_t) const;
+//     std::vector<T> &operator[](size_t);
+// };
 
-template <typename T>
-BinomialLattice<T>::BinomialLattice(size_t newPeriods) : periods(newPeriods) {
-    lattice.resize(periods + 1);
-    for (size_t i = 0; i <= periods; i++) {
-        lattice[i].resize(i + 1);
-    }
-}
+// template <typename T>
+// BinomialLattice<T>::BinomialLattice(size_t newPeriods) : periods(newPeriods)
+// {
+//     lattice.resize(periods + 1);
+//     for (size_t i = 0; i <= periods; i++) {
+//         lattice[i].resize(i + 1);
+//     }
+// }
 
-template <typename T>
-const std::vector<T> &BinomialLattice<T>::operator[](size_t i) const {
-    assert(i <= periods);
-    return lattice[i];
-}
+// template <typename T>
+// const std::vector<T> &BinomialLattice<T>::operator[](size_t i) const {
+//     assert(i <= periods);
+//     return lattice[i];
+// }
 
-template <typename T>
-std::vector<T> &BinomialLattice<T>::operator[](size_t i) {
-    assert(i <= periods);
-    return lattice[i];
-}
+// template <typename T>
+// std::vector<T> &BinomialLattice<T>::operator[](size_t i) {
+//     assert(i <= periods);
+//     return lattice[i];
+// }
 
-template <typename T>
-size_t BinomialLattice<T>::getPeriods() const {
-    return periods;
-}
+// template <typename T>
+// size_t BinomialLattice<T>::getPeriods() const {
+//     return periods;
+// }
 
-template <typename T>
-const std::vector<std::vector<T>> &BinomialLattice<T>::getLattice() const {
-    return lattice;
-}
+// template <typename T>
+// const std::vector<std::vector<T>> &BinomialLattice<T>::getLattice() const {
+//     return lattice;
+// }
 
 template <typename T>
 std::ostream &operator<<(std::ostream &, const std::vector<T> &);
