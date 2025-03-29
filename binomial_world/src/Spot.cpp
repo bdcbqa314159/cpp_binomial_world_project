43c39f4c12a712cb03d44ebd6112d71182ce63da
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-03-29 10:16:47 +0000

    cleaning

--- binomial_world/src/Spot.cpp
+++
@@ -1,9 +0,0 @@
-#include "Spot.hpp"
-
-Spot::Spot(double _value) : value(_value) {}
-double Spot::operator()() const { return value; }
-void Spot::operator()(double _value) { value = _value; }
-std::ostream &operator<<(std::ostream &os, const Spot &spot) {
-    os << spot();
-    return os;
-}
